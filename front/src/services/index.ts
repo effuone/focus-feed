bdc2c760d86b75e756320e7bc346766375be8c1f
effuone <albkfil@gmail.com>
2024-08-25 01:37:59 +0500

    backend url added

---
+++ front/src/services/index.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const backendApiUrl = 'http://localhost:8000/';
+
+const backendApiInstance = axios.create({
+  baseURL: backendApiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+export default backendApiInstance;
