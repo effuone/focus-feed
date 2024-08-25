b3269b10f45462dffa712ac8895a78ea6b350233
bahauddin <baha200477@gmail.com>
2024-08-26 03:04:27 +0500

    basic auth with migration

--- back/src/auth/auth.py
+++ back/src/auth/auth.py
@@ -14,25 +14,39 @@ from .utils import create_access_token, get_password_hash, verify_password
 
 router = APIRouter()
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 64d9d8a (basic auth with migration)
 @router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_async_db)):
     query = select(User).where(User.email == form_data.username)
     result = await db.execute(query)
     user = result.scalars().first()
+<<<<<<< HEAD
     
+=======
+>>>>>>> 64d9d8a (basic auth with migration)
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+<<<<<<< HEAD
     
+=======
+>>>>>>> 64d9d8a (basic auth with migration)
     access_token_expires = timedelta(minutes=30)
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 64d9d8a (basic auth with migration)
 @router.post("/register", response_model=Token)
 async def register_user(email: EmailStr, password: str, db: AsyncSession = Depends(get_async_db)):
     query = select(User).where(User.email == email)
@@ -56,4 +70,8 @@ async def register_user(email: EmailStr, password: str, db: AsyncSession = Depen
 
 @router.get("/me")
 async def read_users_me(current_user: User = Depends(get_current_user)):
+<<<<<<< HEAD
     return current_user
+=======
+    return current_user
+>>>>>>> 64d9d8a (basic auth with migration)
