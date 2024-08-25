9d8e7a5e934b92fd599865c2bb147c214b8b29c5
effuone <65224356+effuone@users.noreply.github.com>
2024-08-25 19:33:20 +0500

    file upload page added, with landing link

---
+++ front/src/app/feed/page.tsx
@@ -0,0 +1,9 @@
+import FileUploadDialog from '@/components/file-upload-dialog';
+
+export default function Feed() {
+  return (
+    <div className='flex justify-center align-center p-4'>
+      <FileUploadDialog />
+    </div>
+  );
+}
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
                 </div>
                 <div className='flex flex-col gap-2 min-[400px]:flex-row'>
                   <Link
-                    href='#demo'
+                    href='/feed'
                     className='inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
                     prefetch={false}
                   >
