ac3c6548c95702380c6c4fa9ef8e1fb89cb56545
bahauddin <baha200477@gmail.com>
2024-08-26 02:33:14 +0500

    basic auth with migration

--- back/src/app/schema.py
+++ back/src/app/schema.py
@@ -1,16 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-    username: str
-    email: str
+    email: EmailStr
     password: str
 
 
 class User(BaseModel):
     id: int
-    username: str
-    email: str
+    email: EmailStr
 
     class Config:
         orm_mode = True
