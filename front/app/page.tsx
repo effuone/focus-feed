b45bc1401ef434e8a8bacbe8b95b270bb99bbf6b
effuone <albkfil@gmail.com>
2024-08-26 20:45:53 +0500

    watch demo link updated

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -251,7 +251,7 @@ export default function Home() {
               <div className='flex justify-center'>
                 {' '}
                 <Link
-                  href='/demo'
+                  href='https://www.loom.com/share/082a208e36044e8db6bddd3e253f3878'
                   className='inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
                   prefetch={false}
                 >
